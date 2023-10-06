32a39291e2cabbffdb3c6ebe6b5df7aa5adc0306
Asuki-Suna <joelmh500@gmail.com>
2023-10-06 15:50:21 +0100

    codigo de la página corregido, falta hacer un login funcional

--- FrontEnd/Biblioteca_Asuki_Suna/src/app/login/login.page.ts
+++ FrontEnd/Biblioteca_Asuki_Suna/src/app/login/login.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-login',
@@ -13,24 +11,10 @@ export class LoginPage implements OnInit {
   ionicForm: FormGroup;
 
   constructor(private router: Router) { }
-  // , public formBuilder: FormBuilder
   ngOnInit() {
 
   }
-  // this.ionicForm = this.formBuilder.group({
-  //   email: ['',[ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),],],
-  //   password: ['', [Validators.required, Validators.pattern('^[a-z0-9]+$')]],
-  // get errorControl() {
-  //   return this.ionicForm.controls;
-  // }
-  // submitForm = () => {
-  //   if (this.ionicForm.valid) {
-  //     console.log(this.ionicForm.value);
-  //     return false;
-  //   } else {
-  //     return console.log('Please provide all the required values!');
-  //   }
-  // };
+
   
   onSubmit(){
     this.router.navigateByUrl("/library")
